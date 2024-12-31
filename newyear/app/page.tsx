00fb0b9e328cca43cf1ae3930fa5cc6c6439216d
Gaurav Kumar Singh <gsingh332211@gmail.com>
2024-12-31 22:19:04 +0530

    Added Vercel Analytics and Speed Insights

--- newyear/app/page.tsx
+++ newyear/app/page.tsx
@@ -1,6 +1,13 @@
 import NewYearCelebration from '@/components/new_year_celebration';
-
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Home() {
-  return <NewYearCelebration />
+  return (
+    <div>
+        <NewYearCelebration />
+        <Analytics/>
+        <SpeedInsights/>
+    </div>
+  )
 }
 
